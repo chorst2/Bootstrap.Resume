410174460f65915e92f77be3ed829a95c3392c66
Casidy Horst <CHorst2@my.wctc.edu>
2021-08-30 12:13:24 -0500

    bootstrap resume

--- resume.js
+++ resume.js
@@ -1,7 +1,2 @@
 //makes the dropdown menu appear
 $('.dropdown-toggle').dropdown()
-
-//for making the dropdown items show up when clicked
-$('#pizzaTab').on('click', function(){
-    $('#portfolio').tab('show')
-})
